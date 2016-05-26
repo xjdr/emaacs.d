e68c7630b46d703b4c6f97587ad50c8ec33cb75f
pdex <christopherkeating@gmail.com>
2016-05-26 18:09:43 -0500

    Fixed typo in backup-directory-alist

--- init.el
+++ init.el
@@ -15,11 +15,11 @@
 
 ;; I prefer my backups sorted elsewhere:
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying      t  ; Don't de-link hard links
-      version-control        t  ; Use version numbers on backups
-      delete-old-versions    t  ; Automatically delete excess backups:
-      kept-new-versions      5  ; how many of the newest versions to keep
-      kept-old-versions      5) ; and how many of the old
+      backup-by-copying       t  ; Don't de-link hard links
+      version-control         t  ; Use version numbers on backups
+      delete-old-versions     t  ; Automatically delete excess backups:
+      kept-new-versions       5  ; how many of the newest versions to keep
+      kept-old-versions       5) ; and how many of the old
 
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
