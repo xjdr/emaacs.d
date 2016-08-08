aed68aa638c7183a03e602cb2f96f3528d4778a8
jefrose <jefrose@paypal.com>
2016-08-08 08:58:42 -0500

    update babel and themes

--- init.el
+++ init.el
@@ -133,15 +133,40 @@
 (setq ispell-program-name "/usr/local/bin/aspell")
 (setq ispell-dictionary "english")
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((ditaa . t)))
-
 (custom-set-variables
  '(org-ditaa-jar-path "/usr/local/Cellar/ditaa/0.9/libexec/ditaa0_9.jar"))
 
 (add-hook 'org-mode-hook 'flyspell-mode)
 (add-hook 'org-mode-hook 'flyspell-buffer)
+(add-hook 'org-mode-hook 'org-src-color-blocks-dark)
+
+(setq org-src-fontify-natively t)
+(setq org-src-tab-acts-natively t)
+
+(org-babel-do-load-languages 'org-babel-load-languages
+                             '((sh         . t)
+                               (java       . t)
+                               (js         . t)
+                               (emacs-lisp . t)
+                               (ditaa      . t)
+                               (scala      . t)
+                               (clojure    . t)
+                               (python     . t)))
+
+
+(defun org-src-color-blocks-dark ()
+  "Colors the block headers and footers to make them stand out more for dark themes"
+  (interactive)
+  (custom-set-faces
+   '(org-block-begin-line
+     ((t (:foreground "#008ED1" :background "#002E41"))))
+   '(org-block-background
+     ((t (:background "#000000"))))
+   '(org-block
+     ((t (:background "#000000"))))
+   '(org-block-end-line
+     ((t (:foreground "#008ED1" :background "#002E41"))))))
+
 
 ;; Flymake
 (defun flymake-display-warning (warning)
@@ -198,7 +223,7 @@
 (defun java-impl-name ()
   "convert from java test name to implementation"
   (concat
-    (replace-regexp-in-string "FunctionalTest\\|IntegrationTest\\|UnitTest" ""
+    (replace-regexp-in-string "FunctionalTest\\|IntegrationTest\\|UnitTest\\|Test" ""
       (replace-regexp-in-string "test/java" "main/java"
         (file-name-sans-extension (buffer-file-name)))) ".java"))
 
@@ -263,18 +288,18 @@
 
 ;; Theme?
 (custom-set-faces
- '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White"))))
+ '(default ((t (:inherit nil :stipple nil :background "#111111" :foreground "White"))))
  '(font-lock-warning-face ((nil (:foreground "#ff6666"))))
  '(highlight ((((class color) (min-colors 88) (background dark)) (:background "#111111"))))
  '(hl-line ((nil (:background "#222222"))))
- '(region ((nil (:background "#255255"))))
+ '(region ((nil (:background "#657489"))))
  '(show-paren-match ((nil (:background "#1793d0"))))
  '(show-paren-mismatch ((((class color)) (:background "red")))))
 
 (set-face-attribute 'mode-line nil
    :foreground "#1793d0"
-   :background "#111111"
-   :box '(:line-width 6 :color "#111111" :style nil))
+   :background "Black"
+   :box '(:line-width 6 :color "Black" :style nil))
 
 (set-face-attribute 'mode-line-inactive nil
    :foreground "#1793d0"
